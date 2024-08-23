60d56838ec61347543772bdea6ca7abdeabef45a
Hossein <htaheri550@gmail.com>
2024-08-23 18:36:31 +0330

    Some codes in creating dataset have been refactored

--- creating_dataset/commands/change_datasets_format.py
+++ creating_dataset/commands/change_datasets_format.py
@@ -13,36 +13,7 @@ class ChangeDatasetsFormat(BaseCommand):
         pass
 
     def execute(self):
-        def change_datasets_format(data_paths):
-            for paths in data_paths:
-                with open(paths[0], 'r') as file:
-                    lines = file.readlines()
-                user_assistant_dataset = []
-                for prompt_answer_pair in lines:
-                    prompt_answer_pair = json.loads(prompt_answer_pair)
-                    user_assistant_dataset.append({
-                        "messages": [
-                            {
-                                "role": "system",
-                                "content": "You are an empathetic medical assistant to help detection of disease.",
-                            },
-                            {
-                                "role": "user",
-                                "content": prompt_answer_pair['prompt'],
-                            },
-                            {
-                                "role": "assistant",
-                                "content": prompt_answer_pair['completion'],
-                            },
-                        ]
-                    })
-
-                with open(paths[1], 'w') as file:
-                    for item in user_assistant_dataset:
-                        json.dump(item, file)
-                        file.write('\n')
-
-        change_datasets_format(
+        ChangeDatasetsFormat.change_datasets_format(
             data_paths=[
                 (
                     "creating_dataset/data/qa_raw_data/json_qa_dataset.jsonl",
@@ -54,6 +25,35 @@ class ChangeDatasetsFormat(BaseCommand):
                 )
             ]
         )
+
+    @staticmethod
+    def change_datasets_format(data_paths):
+        for paths in data_paths:
+            with open(paths[0], 'r') as file:
+                lines = file.readlines()
+            user_assistant_dataset = []
+            for prompt_answer_pair in lines:
+                prompt_answer_pair = json.loads(prompt_answer_pair)
+                user_assistant_dataset.append({
+                    "messages": [
+                        {
+                            "role": "system",
+                            "content": "You are an empathetic medical assistant to help detection of disease.",
+                        },
+                        {
+                            "role": "user",
+                            "content": prompt_answer_pair['prompt'],
+                        },
+                        {
+                            "role": "assistant",
+                            "content": prompt_answer_pair['completion'],
+                        },
+                    ]
+                })
+            with open(paths[1], 'w') as file:
+                for item in user_assistant_dataset:
+                    json.dump(item, file)
+                    file.write('\n')
 
     def cleanup(self):
         pass
--- creating_dataset/commands/split_datasets.py
+++ creating_dataset/commands/split_datasets.py
@@ -13,30 +13,7 @@ class SplitDatasets(BaseCommand):
         pass
 
     def execute(self):
-        def split_jsonl(file_paths, train_ratio=0.90, val_ratio=0.05):
-            with open(file_paths['main_dataset'], 'r') as file:
-                lines = file.readlines()
-
-            random.shuffle(lines)
-
-            train_split_index = int(len(lines) * train_ratio)
-            val_split_index = int(len(lines) * (train_ratio + val_ratio))
-            train_lines = lines[:train_split_index]
-            val_lines = lines[train_split_index:val_split_index]
-            test_lines = lines[val_split_index:]
-
-            with open(file_paths['train_dataset'], 'w') as train_file:
-                train_file.writelines(train_lines)
-
-            with open(file_paths['validation_dataset'], 'w') as val_file:
-                val_file.writelines(val_lines)
-
-            with open(file_paths['test_dataset'], 'w') as val_file:
-                val_file.writelines(test_lines)
-
-            print(f"Data split into {len(train_lines)} training and {len(val_lines)} validation lines.")
-
-        split_jsonl(
+        SplitDatasets.split_jsonl(
             {
                 'main_dataset': 'creating_dataset/data/qa_raw_data/qa_dataset.jsonl',
                 'train_dataset': 'creating_dataset/data/qa_raw_data/qa_dataset_train.jsonl',
@@ -47,3 +24,27 @@ class SplitDatasets(BaseCommand):
 
     def cleanup(self):
         pass
+
+    @staticmethod
+    def split_jsonl(file_paths, train_ratio=0.90, val_ratio=0.05):
+        with open(file_paths['main_dataset'], 'r') as file:
+            lines = file.readlines()
+
+        random.shuffle(lines)
+
+        train_split_index = int(len(lines) * train_ratio)
+        val_split_index = int(len(lines) * (train_ratio + val_ratio))
+        train_lines = lines[:train_split_index]
+        val_lines = lines[train_split_index:val_split_index]
+        test_lines = lines[val_split_index:]
+
+        with open(file_paths['train_dataset'], 'w') as train_file:
+            train_file.writelines(train_lines)
+
+        with open(file_paths['validation_dataset'], 'w') as val_file:
+            val_file.writelines(val_lines)
+
+        with open(file_paths['test_dataset'], 'w') as val_file:
+            val_file.writelines(test_lines)
+
+        print(f"Data split into {len(train_lines)} training and {len(val_lines)} validation lines.")
